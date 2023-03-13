ad71080949914fa5f9558b48cc456cbb2a2d4c06
Zhanet <zhanet_f@mail.ru>
2023-03-13 23:35:19 +0300

    feat: dialogPage layout

--- src/features/contacts/Contacts.tsx
+++ src/features/contacts/Contacts.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import {Layout} from "antd";
+import { Layout } from "antd";
 
-const {Content}=Layout;
+const {Content} = Layout;
 
 export const Contacts = () => {
     return (
         <div>
-            <Content style={{
-                background: '#001529',
-                borderRadius: 5,
-                margin: '10px 16px',
-                padding: 24,
-            }}>
-                <h1 style={{color: '#d7d7a7'}}>Users</h1>
+            <Content>
+                Users
             </Content>
         </div>
     );
--- src/features/dialogs/Dialogs.tsx
+++ src/features/dialogs/Dialogs.tsx
@@ -1,19 +1,50 @@
 import React from 'react';
-import {Layout} from "antd";
+import {Col, Layout, List, Row} from "antd";
 
 const {Content} = Layout;
+
+const fakeData = [
+    'Anna',
+    'Lena',
+    'Sveta',
+    'Dima',
+    'Sergey'
+
+];
 
 export const Dialogs = () => {
     return (
         <div>
-            <Content style={{
-                background: '#001529',
-                borderRadius: 5,
-                margin: '10px 16px',
-                padding: 24,
-            }}>
-                <h1 style={{color: '#d7d7a7'}}>Dialogs</h1>
-            </Content>
+            <Row gutter={8}>
+                <Col flex={4}>
+                    <Content style={{
+                        background: '#001529',
+                        borderRadius: 5,
+                        margin: '10px 16px',
+                        padding: 24,
+                    }}>
+                        <h1 style={{color: '#d7d7a7'}}>Dialogs</h1>
+                    </Content>
+                </Col>
+                <Col flex={1}>
+                    <Content style={{
+                        background: '#f5f5f5',
+                        borderRadius: 5,
+                        margin: '10px 16px',
+
+                    }}>
+
+                        <List
+                            size="small"
+                            header={<h1 style={{color: '#001529'}}>Users</h1>}
+                            bordered
+                            dataSource={fakeData}
+                            renderItem={(item) => <List.Item>{item}</List.Item>}
+                        />
+
+                    </Content>
+                </Col>
+            </Row>
         </div>
     );
 };
